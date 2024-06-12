c6a0993a2ede112cc2c401cb859aa878119909ac
Shivam Kumar <shivamkr.kr23@gmail.com>
2024-06-12 14:15:03 +0530

    fix(tmux): now run tmux-sessionizer and tmux-cht.sh attaching to tmux

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -6,9 +6,14 @@ fi
 
 read -p "Enter Query: " query
 
+
 if grep -qs "$selected" ~/.local/scripts/tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | bat"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
+
+if [[ -z $TMUX ]]; then
+    tmux a
 fi
